9314a184fc96773809d6d592bfcc6c9449be042d
Raajheer1 <raaj.patel229@gmail.com>
2023-08-05 15:16:38 -0500

    Delete handler debug code

--- pkg/api/submit.pb.go
+++ pkg/api/submit.pb.go
@@ -1964,16 +1964,7 @@ func _Submit_SubmitJobs_Handler(srv interface{}, ctx context.Context, dec func(i
 		FullMethod: "/api.Submit/SubmitJobs",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		a1, b1 := srv.(SubmitServer).SubmitJobs(ctx, req.(*JobSubmitRequest))
-		st := status.Convert(b1)
-		fmt.Println("gRPC Details - 1:", st.Details())
-		msgType := proto.MessageType("api.JobSubmitResponse")
-		fmt.Println("Registered type:", msgType)
-
-		msgType2 := proto.MessageType("api.JobSubmitResponseItem")
-		fmt.Println("Registered type:", msgType2)
-
-		return a1, b1
+		return srv.(SubmitServer).SubmitJobs(ctx, req.(*JobSubmitRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
